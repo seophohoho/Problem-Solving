6d622be56201dea6ce4746ea13ec57758f4de19d
seophohoho <123441904+seophohoho@users.noreply.github.com>
2025-01-04 15:57:15 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14744 KB -BaekjoonHub

---
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    private static int[][] maze;
+    private static boolean[][] visited;
+    private static int N, M;
+
+    private static final int[] dx = {-1, 1, 0, 0};
+    private static final int[] dy = {0, 0, -1, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputs = br.readLine().split(" ");
+        N = Integer.parseInt(inputs[0]);
+        M = Integer.parseInt(inputs[1]);
+
+        maze = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < M; j++) {
+                maze[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        int result = bfs(0, 0);
+        System.out.println(result);
+    }
+
+    private static int bfs(int startX, int startY) {
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{startX, startY, 1});
+        visited[startX][startY] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            int result = current[2];
+
+            if (x == N - 1 && y == M - 1) {
+                return result;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                    if (maze[nx][ny] == 1 && !visited[nx][ny]) {
+                        queue.add(new int[]{nx, ny, result+1});
+                        visited[nx][ny] = true;
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
