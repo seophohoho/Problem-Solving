7f0b11e2b288649254acbedc5fc3c24ee20567a9
seophohoho <123441904+seophohoho@users.noreply.github.com>
2025-06-08 14:53:33 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.23 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] result = my_string.split(" ");
+        
+        return result;
+    }
+}
